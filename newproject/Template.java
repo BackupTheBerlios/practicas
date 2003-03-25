0f6fed8e8e547d68aa037c37c82f86176749d846
mariscal <mariscal>
2003-03-25 20:05:57 +0000

    Initial revision

---
+++ newproject/Template.java
@@ -0,0 +1,82 @@
+/*
+ * @(#)__NAME__.java
+ *
+ * @INFO_1@
+ * @INFO_2@
+ * @INFO_3@
+ * @INFO_4@
+ *
+ * =============================================================================
+ *
+ * Este programa es software libre. Puede redistribuirlo y/o modificarlo bajo
+ * los términos de la Licencia Pública General de GNU según es publicada por la
+ * Free Software Foundation, bien de la versión 2 de dicha Licencia o bien
+ * (según su elección) de cualquier versión posterior.
+ *
+ * Este programa se distribuye con la esperanza de que sea útil, pero SIN
+ * NINGUNA GARANTÍA, incluso sin la garantía MERCANTIL implícita o sin
+ * garantizar la CONVENIENCIA PARA UN PROPÓSITO PARTICULAR. Véase la Licencia
+ * Pública General de GNU para más detalles.
+ *
+ * Debería haber recibido una copia de la Licencia Pública General junto con
+ * este programa. Si no ha sido así, escriba a la Free Software Foundation, Inc.,
+ * en 675 Mass Ave, Cambridge, MA 02139, EEUU.
+ *
+ * =============================================================================
+ *
+ * $Id: Template.java,v 1.1 2003/03/25 20:05:57 mariscal Exp $
+ *
+ * Fecha de creación:             __DATE__, __TIME__
+ * Fecha de última modificación:  $Date: 2003/03/25 20:05:57 $
+ *
+ * Historial de cambios:
+ *
+ * $Log: Template.java,v $
+ * Revision 1.1  2003/03/25 20:05:57  mariscal
+ * Initial revision
+ *
+ *
+ * =============================================================================
+ */
+
+package Templates.Classes.uc3m;
+
+/**
+ *
+ * @author	@AUTHOR@ 
+ * @version $Revision: 1.1 $, $Date: 2003/03/25 20:05:57 $, by $Author: mariscal $
+ * @see		@URL@ 
+ */
+public class Template {
+    
+    /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::                          ATRIBUTOS                                 ::*/
+    /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    
+    /*::::::::::::::::::::::::::: Privados :::::::::::::::::::::::::::::::::::*/
+    
+    /*:::::::::::::::::::::::::: Constantes ::::::::::::::::::::::::::::::::::*/
+    
+    /*::::::::::::::::::::::::::: Públicos :::::::::::::::::::::::::::::::::::*/
+    
+    
+    /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    /*::                           MÉTODOS                                  ::*/
+    /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    
+    /*:::::::::::::::::::::::: Constructores :::::::::::::::::::::::::::::::::*/
+    
+    /** Constructor por defecto. Construye una nueva instancia */
+    public Template() {
+    }
+    
+    
+    /*:::::::::::::::::::::::::: Métodos GET :::::::::::::::::::::::::::::::::*/
+    
+    /*:::::::::::::::::::::::::: Métodos SET :::::::::::::::::::::::::::::::::*/
+    
+    /*::::::::::::::::::::::::::: Privados :::::::::::::::::::::::::::::::::::*/
+    
+    /*::::::::::::::::::::::::::: Públicos :::::::::::::::::::::::::::::::::::*/
+    
+}
